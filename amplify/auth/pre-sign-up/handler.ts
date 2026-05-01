8f187830d71c773dfe2ddeedac5910f4e5262948
Edko03 <121055810+EidrianGospodinov@users.noreply.github.com>
2026-05-01 14:07:37 +0100

    allowing only westminster domain to register- authentication

---
+++ amplify/auth/pre-sign-up/handler.ts
@@ -0,0 +1,12 @@
+﻿import type { PreSignUpTriggerHandler } from "aws-lambda";
+
+export const handler: PreSignUpTriggerHandler = async (event) => {
+    const email = event.request.userAttributes.email;
+    console.log("PRE SIGN UP TRIGGER RUNNING");
+    console.log("EMAIL:", email);
+    if (!email?.endsWith("@westminster.ac.uk")) {
+        throw new Error("Only westminster.ac.uk accounts are allowed.");
+    }
+
+    return event;
+};
